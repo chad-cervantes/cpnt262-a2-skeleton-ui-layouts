971ee4eacd230e8f68da5d8c031813d4e8f5bcc2
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 15:36:41 -0700

    added About page in routes

---
+++ src/routes/About/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { AppShell } from "@skeletonlabs/skeleton";
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+    <div class="text-center text-5xl p-4">
+      About
+    </div>
+  </svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
