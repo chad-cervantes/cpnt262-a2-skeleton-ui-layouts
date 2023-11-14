6ca3a5e7a5d0eb6698dd4b8974d174cd3c4a8faa
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 13:18:57 -0700

    added Home folder in routes for home page

---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Footer from "../../lib/components/Footer.svelte";
+  import Header from "../../lib/components/Header.svelte";
+  import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<Header />
+
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+</AppShell>
+
+<Footer />
