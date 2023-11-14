b695201357423aece84987adaffc9bc0ec968183
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 16:40:01 -0700

    added wikipedia  image as hero-section image

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -8,6 +8,9 @@
 			Day of The Dead
 		</div>
 	</svelte:fragment>
+	<div class="m-auto p-4 flex align-center">
+		<img src="/images/day-of-the-dead-wikipedia.jpg" alt=""/>
+	</div>
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
 	<!-- Router Slot -->
