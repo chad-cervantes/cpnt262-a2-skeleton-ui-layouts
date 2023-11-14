ffb8252241827da0d7f853ad4ce739ab4313a2b7
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 15:39:17 -0700

    changed header title to Day of the Dead

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <AppShell>
-	<svelte:fragment slot="header">Home</svelte:fragment>
+	<svelte:fragment slot="header">
+		<div class="text-center text-5xl p-4">
+			Day of The Dead
+		</div>
+	</svelte:fragment>
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
 	<!-- Router Slot -->
