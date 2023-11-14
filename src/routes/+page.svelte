f78aca638d6c9df79aa6ee528af504b7bf823223
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 13:21:05 -0700

    imported Footer.svelte and added it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
 
 <script>
   import '../app.postcss';
+	import Footer from '../lib/components/Footer.svelte';
   import Header from '../lib/components/Header.svelte';
 	
 </script>
 
 <Header />
+<Footer />
 
 
 <!-- <div class="container h-full mx-auto flex justify-center items-center">
