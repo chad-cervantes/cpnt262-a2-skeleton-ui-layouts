dd45b0ce16ae221a7685d4ca854782101116fc93
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 15:36:14 -0700

    added History page in routes

---
+++ src/routes/History/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { AppShell } from "@skeletonlabs/skeleton";
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+    <div class="text-center text-5xl p-4">
+      History
+    </div></svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
