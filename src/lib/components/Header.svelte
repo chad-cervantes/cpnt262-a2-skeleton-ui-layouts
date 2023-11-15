2c614abb48043d7a481440db048e8cd77705837e
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:39:55 -0700

    added Blog tab link to Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,18 +10,23 @@
     <AppBar gridColumns="grid-cols-1">
     <div class="lg:flex justify-between gap-4 p-4">
 	    <div>
-        <a href="/Home" class="flex items-center">
+        <span class="flex items-center">
             <Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-10" height="h-10" rounded="rounded-full" />
           <div>
             <span class="text-xl p-1 block">Day of the Dead</span>
           </div> 
-        </a>
+        </span>
       </div>
       <nav>
       <ul class="lg:flex list-none gap-4">
         <li>  
           <a class="btn btn-sm variant-ghost-surface" href="/Home">
             Home
+          </a>
+        </li>
+        <li>  
+          <a class="btn btn-sm variant-ghost-surface" href="/Blog">
+            Blog
           </a>
         </li>
         <li>  
