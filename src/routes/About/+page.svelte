c9cb89941b36ef90cd7b52347bb685825e208190
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:54:39 -0700

    reduced the width and centered the text

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -5,7 +5,7 @@
 
 <!-- <AppShell> -->
 	<!-- <svelte:fragment slot="header"> -->
-    <h1 class="text-center text-5xl p-4  bg-primary-400 text-secondary-900">
+    <h1 class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
       About
     </h1>
   <!-- </svelte:fragment> -->
@@ -13,10 +13,7 @@
 	<!-- (sidebarRight) -->
 	<!-- (pageHeader) -->
 	<!-- Router Slot -->
-  <div class="p-4 text-left text-l mb-4 pb-4">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <div class="p-4 text-left text-xl m-auto mb-4 pb-4 w-[75%]">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
