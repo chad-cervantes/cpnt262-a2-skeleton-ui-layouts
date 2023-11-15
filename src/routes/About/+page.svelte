f016907bed75c5874766a3ff317f18558e45e971
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:20:08 -0700

    got rid of AppShell in About

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,14 +1,14 @@
-<script>
+<!-- <script>
   import { AppShell } from "@skeletonlabs/skeleton";
-</script>
+</script> -->
 
 
-<AppShell>
-	<svelte:fragment slot="header">
-    <div class="text-center text-5xl p-4  bg-primary-400 text-secondary-900">
+<!-- <AppShell> -->
+	<!-- <svelte:fragment slot="header"> -->
+    <h1 class="text-center text-5xl p-4  bg-primary-400 text-secondary-900">
       About
-    </div>
-  </svelte:fragment>
+    </h1>
+  <!-- </svelte:fragment> -->
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
 	<!-- (pageHeader) -->
@@ -26,8 +26,8 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
   </div>
 
-	<slot />
+	<!-- <slot /> -->
 	<!-- ---- / ---- -->
 	<!-- (pageFooter) -->
 	<!-- (footer) -->
-</AppShell>
+<!-- </AppShell> -->
