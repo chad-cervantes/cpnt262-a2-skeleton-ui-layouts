c547cd1d58ef92028aed0809723111759630340e
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:42:24 -0700

    added content to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,28 +3,22 @@
 <script>  
 	import { AppShell } from '@skeletonlabs/skeleton';
   import '../app.postcss';
-	import '../routes/Home/+page.svelte';
-	import '../routes/Gallery/+page.svelte';
-	import '../routes/History/+page.svelte';
-	import '../routes/About/+page.svelte';
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<div class="text-center text-5xl p-4">
+		<div class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
 			Day of The Dead
 		</div>
 	</svelte:fragment>
-	<div class="m-auto p-4 w-full lg:flex justify-center items-centerr">
-		<img src="/images/day-of-the-dead-wikipedia.jpg" alt=""/>
+	<div class="m-auto p-4 w-full lg:flex justify-center items-center">
+		<img src="/images/day-of-the-dead-wikipedia.jpg" alt="day of the dead drawing"/>
 	</div>
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
 	<!-- Router Slot -->
 	<slot />
 </AppShell>
-
-
 
 
 <!-- <div class="container h-full mx-auto flex justify-center items-center">
