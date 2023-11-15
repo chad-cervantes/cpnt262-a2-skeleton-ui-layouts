e25fe7f3ca318424a564e5835ef4bcb63b87bcb7
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 16:37:26 -0700

    added content to Blog +page.svelte

--- src/routes/Blog/+page.svelte
+++ src/routes/Blog/+page.svelte
@@ -1,19 +1,14 @@
-<script>
-  import { AppShell } from '@skeletonlabs/skeleton';
-</script>
-
-<AppShell>
-	<svelte:fragment slot="header">
-		<div class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
-			Day of The Dead
-		</div>
-	</svelte:fragment>
-	<div class="m-auto p-4 w-full lg:flex justify-center items-center">
-		<img src="/images/day-of-the-dead-wikipedia.jpg" alt="day of the dead drawing"/>
-	</div>
-	<!-- (sidebarLeft) -->
-	<!-- (sidebarRight) -->
-	<!-- Router Slot -->
-	<slot />
-</AppShell>
-
+<section class="grid grid-cols 2 p-4">
+<h1>Blog Post 1</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<h1>Blog Post 2</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<h1>Blog Post 3</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<h1>Blog Post 4</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<h1>Blog Post 5</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<h1>Blog Post 6</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+</section>
