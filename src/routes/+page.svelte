6430b3a06fc8ac7c38f05d305ce7648ec32f6413
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 19:44:03 -0700

    added content to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<script>
+<script>  
+	import { AppShell } from '@skeletonlabs/skeleton';
   import '../app.postcss';
 	import '../routes/Home/+page.svelte';
 	import '../routes/Gallery/+page.svelte';
 	import '../routes/History/+page.svelte';
 	import '../routes/About/+page.svelte';
 </script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<div class="text-center text-5xl p-4">
+			Day of The Dead
+		</div>
+	</svelte:fragment>
+	<div class="m-auto p-4 flex align-center">
+		<img src="/images/day-of-the-dead-wikipedia.jpg" alt=""/>
+	</div>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- Router Slot -->
+	<slot />
+</AppShell>
+
+
 
 
 <!-- <div class="container h-full mx-auto flex justify-center items-center">
