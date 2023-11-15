f2b0d49e21081d6153a836715a254fdda60c6b4f
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:53:41 -0700

    fixed indentation and spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,9 +14,6 @@
 	<div class="m-auto p-4 w-full lg:flex justify-center items-center">
 		<img src="/images/day-of-the-dead-wikipedia.jpg" alt="day of the dead drawing"/>
 	</div>
-	<!-- (sidebarLeft) -->
-	<!-- (sidebarRight) -->
-	<!-- Router Slot -->
 	<slot />
 </AppShell>
 
