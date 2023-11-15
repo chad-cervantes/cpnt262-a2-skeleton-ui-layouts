143bf200b4eac329e8805b4abd1dbe571c246f25
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 16:34:21 -0700

    deleted AppShell in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,15 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <script>  
-	import { AppShell } from '@skeletonlabs/skeleton';
   import '../app.postcss';
 </script>
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<div class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
-			Day of The Dead
-		</div>
-	</svelte:fragment>
+	<h1 class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
+		Day of The Dead
+	</h1>
 	<div class="m-auto p-4 w-full lg:flex justify-center items-center">
 		<img src="/images/day-of-the-dead-wikipedia.jpg" alt="day of the dead drawing"/>
 	</div>
-	<slot />
-</AppShell>
 
 
 <!-- <div class="container h-full mx-auto flex justify-center items-center">
