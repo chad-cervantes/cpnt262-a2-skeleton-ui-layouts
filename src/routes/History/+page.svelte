8ff7c1339808de79d2c7c95ae8ecd66aa0622fef
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 15:59:31 -0700

    reduced the width and centered lorem ipsum text

--- src/routes/History/+page.svelte
+++ src/routes/History/+page.svelte
@@ -13,10 +13,7 @@
 	<!-- (sidebarRight) -->
 	<!-- (pageHeader) -->
 	<!-- Router Slot -->
-  <div class="p-4 text-left text-l mb-4 pb-4">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <div class="p-4 text-left text-xl m-auto mb-4 pb-4 w-[75%]">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
