87f4bd915b09522e90e707b5121477468e3de2eb
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 16:35:57 -0700

    added secondary layout for Blog +layout.svelte

---
+++ src/routes/Blog/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+    <div class="text-5xl text-center p-4">
+      Blog
+    </div>
+  </svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">
+    <div class="text-2xl text-center p-4">
+      November Posts
+    </div>
+  </svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
