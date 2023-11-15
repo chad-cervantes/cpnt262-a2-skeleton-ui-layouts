4509f67f1c2bbdcda32d6622f6d0cf10ada69e09
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-14 19:44:54 -0700

    imported PageFooter.svelte to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
 	import Footer from '../lib/components/Footer.svelte';
+	import PageFooter from '../lib/components/PageFooter.svelte';
 </script>
 
 <Header />
 <slot />
+<PageFooter />
 <Footer />
