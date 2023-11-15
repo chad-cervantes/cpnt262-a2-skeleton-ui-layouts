6cf7e138fa7df1c456e31b879137ddb70443b1e9
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 13:19:57 -0700

    forgot to make alt tag for one of the images

--- src/routes/Gallery/+page.svelte
+++ src/routes/Gallery/+page.svelte
@@ -18,7 +18,7 @@
   <section class="grid grid-cols-2 lg:grid-cols-4 gap-2 p-4">
     <div class="grid gap-4">
       <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" />
+        <img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt=""/>
       </div>
       <div>
         <img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1597077962467-be16edcc6a43?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2MzZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max" alt="" />
