5db1785f4e5a15d72dd3baa1c810a6502df36e76
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-15 21:04:39 -0700

    made Custom.svelte as a custom list component

---
+++ src/lib/components/Custom.svelte
@@ -0,0 +1,10 @@
+<h3 class="text-center text-2xl">List</h3>
+<div class="w-[50%] m-auto">
+<select class="select" size="4" value="1">
+	<option value="1">Lorem Ipsum</option>
+	<option value="2">Lorem Ipsum</option>
+	<option value="3">Lorem Ipsum</option>
+	<option value="4">Lorem Ipsum</option>
+	<option value="5">Lorem Ipsum</option>
+</select>
+</div>					
