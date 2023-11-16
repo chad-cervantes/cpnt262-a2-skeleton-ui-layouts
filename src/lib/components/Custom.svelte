193321843bda95fc455f87b8a95a1ca40a4ef2c3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-16 13:35:33 -0700

    used one of the listboxes from Skeleton UI for my object array, but it gave me undefined

--- src/lib/components/Custom.svelte
+++ src/lib/components/Custom.svelte
@@ -9,19 +9,19 @@ let lists = [
 
 </script>
 
-<section class="text-center text-xl p-4 traking-wide">
+<section class="text-center text-xl p-4 tracking-wide strong">
 	{#each lists as list (list.id)}
 		<list id={list.id}>{list.title}</list>
 	{/each}
 </section>
 
-<!-- <h3 class="text-center text-2xl">List</h3>
+<h3 class="text-center text-2xl">List</h3>
 <div class="w-[50%] m-auto">
 <select class="select" size="4" value="1">
-	<option value="1">Lorem Ipsum</option>
-	<option value="2">Lorem Ipsum</option>
-	<option value="3">Lorem Ipsum</option>
-	<option value="4">Lorem Ipsum</option>
-	<option value="5">Lorem Ipsum</option>
+	<option value="1">{lists.title}</option>
+	<option value="2">{lists.title}</option>
+	<option value="3">{lists.title}</option>
+	<option value="4">{lists.title}</option>
+	<option value="5">{lists.title}</option>
 </select>
-</div>					 -->
+</div>					
