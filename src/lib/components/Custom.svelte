eb8cb2708cf09fbcd47afe76e717ddb55ab9514e
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-16 12:11:16 -0700

    made my list array

--- src/lib/components/Custom.svelte
+++ src/lib/components/Custom.svelte
@@ -1,4 +1,21 @@
-<h3 class="text-center text-2xl">List</h3>
+<script>
+let lists = [
+    { id: 1, title: 'Lorem Ipsum 1' },
+    { id: 2, title: 'Lorem Ipsum 2' },
+    { id: 3, title: 'Lorem Ipsum 3' },
+    { id: 4, title: 'Lorem Ipsum 4' },
+    { id: 5, title: 'Lorem Ipsum 5' },
+  ];
+
+</script>
+
+<section class="text-center text-xl p-4 traking-wide">
+	{#each lists as list (list.id)}
+		<list id={list.id}>{list.title}</list>
+	{/each}
+</section>
+
+<!-- <h3 class="text-center text-2xl">List</h3>
 <div class="w-[50%] m-auto">
 <select class="select" size="4" value="1">
 	<option value="1">Lorem Ipsum</option>
@@ -7,4 +24,4 @@
 	<option value="4">Lorem Ipsum</option>
 	<option value="5">Lorem Ipsum</option>
 </select>
-</div>					
+</div>					 -->
