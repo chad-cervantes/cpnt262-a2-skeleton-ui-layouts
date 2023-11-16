6879ed6fa081f93d817b375a7cca0fd0d09c3bff
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-16 14:37:55 -0700

    fixed the indentation of Blog +page.svelte & organized the text format

--- src/routes/Blog/+page.svelte
+++ src/routes/Blog/+page.svelte
@@ -1,14 +1,14 @@
-<section class="grid grid-cols 2 p-4">
-<h1>Blog Post 1</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-<h1>Blog Post 2</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-<h1>Blog Post 3</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-<h1>Blog Post 4</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-<h1>Blog Post 5</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-<h1>Blog Post 6</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+<section class="w-[50%] m-auto grid grid-cols 2 p-4 pb-4 gap-4">
+  <h1>Blog Post 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+  <h1>Blog Post 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+  <h1>Blog Post 3</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+  <h1>Blog Post 4</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+  <h1>Blog Post 5</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
+  <h1>Blog Post 6</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
 </section>
