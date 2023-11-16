c0fb4f22ba242918f92ace7b798b929b6cabdd13
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-16 09:50:35 -0700

    changed to title to 'Website Title' beside the avatar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 			<span class="flex items-center">
 					<Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-10" height="h-10" rounded="rounded-full" />
 				<div>
-					<span class="text-xl p-1 block">Day of the Dead</span>
+					<span class="text-xl p-1 block">Website Title</span>
 				</div> 
 			</span>
 		</div>
