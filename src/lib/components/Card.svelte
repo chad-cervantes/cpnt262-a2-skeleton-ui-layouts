c692e60dcbf9c94a291714a081b59d78d03882ed
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 11:41:52 -0700

    added Card.svelte for Blog to make blog posts cards

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title;
+  export let description;
+  export let url;
+</script>
+
+<section class="border border-2 mb-4 mr-4 gap-4 p-4">
+  <article class="w-[50%] m-auto p-4 pb-4 gap-4">
+    <a href={url}>
+      <h3 class="text-2xl">{title}</h3>
+      <p class="text-l">{description}</p>
+    </a>
+  </article>
+</section>
