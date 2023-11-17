6651eaa7f63bb3449fa84b5261bf5f84ab2940e8
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 14:30:24 -0700

    deleted commented out code

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,18 +1,7 @@
-<!-- <script>
-  import { AppShell } from "@skeletonlabs/skeleton";
-</script> -->
+<h1 class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
+    About
+</h1>
 
-
-<!-- <AppShell> -->
-	<!-- <svelte:fragment slot="header"> -->
-    <h1 class="text-center text-5xl p-4 bg-primary-400 text-secondary-900">
-      About
-    </h1>
-  <!-- </svelte:fragment> -->
-	<!-- (sidebarLeft) -->
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
   <div class="p-4 text-left text-xl m-auto mb-4 pb-4 w-[75%]">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
@@ -22,9 +11,3 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
   </div>
-
-	<!-- <slot /> -->
-	<!-- ---- / ---- -->
-	<!-- (pageFooter) -->
-	<!-- (footer) -->
-<!-- </AppShell> -->
--- src/routes/History/+page.svelte
+++ src/routes/History/+page.svelte
@@ -1,29 +1,13 @@
-<!-- <script>
-  import { AppShell } from "@skeletonlabs/skeleton";
-</script> -->
+<h1 class="text-center text-5xl p-4  bg-primary-400 text-secondary-900">
+  History
+</h1>
 
-
-<!-- <AppShell> -->
-	<!-- <svelte:fragment slot="header"> -->
-    <h1 class="text-center text-5xl p-4  bg-primary-400 text-secondary-900">
-     History
-    </h1>
-  <!-- </svelte:fragment> -->
-	<!-- (sidebarLeft) -->
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
-  <div class="p-4 text-left text-xl m-auto mb-4 pb-4 w-[75%]">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
-  </div>
-	<!-- <slot /> -->
-	<!-- ---- / ---- -->
-	<!-- (pageFooter) -->
-	<!-- (footer) -->
-<!-- </AppShell> -->
+<div class="p-4 text-left text-xl m-auto mb-4 pb-4 w-[75%]">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora doloribus, necessitatibus molestiae, illo atque voluptas sunt repellat doloremque iure excepturi. Tenetur obcaecati ex quasi, delectus aliquam dolor expedita hic.</p>
+</div>
