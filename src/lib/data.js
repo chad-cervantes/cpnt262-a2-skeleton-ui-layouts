92bea32356809ea504bba3701ce231f066fef0f4
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 11:41:22 -0700

    added data.js for Card.svelte information

---
+++ src/lib/data.js
@@ -0,0 +1,28 @@
+export const cardInfo = [
+
+{title: 'Blog Post 1',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+{title: 'Blog Post 2',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+{title: 'Blog Post 3',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+{title: 'Blog Post 4',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+{title: 'Blog Post 5',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+{title: 'Blog Post 6',
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.',
+url: ''},
+
+
+]
