904cacc3014dbf25d659b52823e4e5fa46aaeab0
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 08:33:44 -0700

    deleted commented out code; adjusted the width and text of the left sidebar

--- src/routes/Blog/+layout.svelte
+++ src/routes/Blog/+layout.svelte
@@ -12,7 +12,7 @@
     </div>
   </svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-    <div class="overflow-x-hidden overflow-y-auto p-4">
+    <div class="sm:text-sm overflow-x-hidden overflow-y-auto p-4 w-[85%] m-auto">
       <h1 class="text-center text-2xl p-4">Categories</h1>
       <ListBox>
         <ListBoxItem bind:group={valueSingle} name="medium" value="books">Costumes</ListBoxItem>
@@ -21,23 +21,11 @@
         <ListBoxItem bind:group={valueSingle} name="medium" value="mobile">Colors</ListBoxItem>
       </ListBox>
 		</div>	
-     <!-- <ul class="p-4">
-      <li>Costumes</li>
-      <li>Decorations</li>
-      <li>Themes</li>
-      <li>Colors</li>
-     </ul> -->
- 
  </svelte:fragment>
-	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
 	<svelte:fragment slot="pageHeader">
     <div class="text-2xl text-center p-4">
       November Posts
     </div>
   </svelte:fragment>
-	<!-- Router Slot -->
 	<slot />
-	<!-- ---- / ---- -->
-	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-	<svelte:fragment slot="footer">Footer</svelte:fragment> -->
 </AppShell>
