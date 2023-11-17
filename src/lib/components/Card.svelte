86ad2b2cfa8ebac88320f09cc67d1c29083c5213
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 14:03:19 -0700

    made Card.svelte for the custom component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -4,11 +4,10 @@
   export let url;
 </script>
 
-<section class="border border-2 mb-4 mr-4 gap-4 p-4">
-  <article class="w-[50%] m-auto p-4 pb-4 gap-4">
+  <article class="w-[60%] border-[2px] border-red-600 border-2 m-auto mb-4 p-4 pb-4 hover:opacity-75 transition ease-linear">
     <a href={url}>
-      <h3 class="text-2xl">{title}</h3>
-      <p class="text-l">{description}</p>
+      <h3 class="text-2xl text-center p-4">{title}</h3>
+      <p class="text-md text-center">{description}</p>
     </a>
   </article>
-</section>
+		
