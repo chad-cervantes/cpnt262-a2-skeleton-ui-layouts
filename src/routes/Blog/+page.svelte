2ef6c904553e555ba36d8216106eab49b5efc793
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 11:42:37 -0700

    imported Card.svelte & data.js , and made a for each loop to render cards

--- src/routes/Blog/+page.svelte
+++ src/routes/Blog/+page.svelte
@@ -1,4 +1,16 @@
-<section class="w-[50%] m-auto lg:grid grid-cols-2 p-4 pb-4 gap-4">
+<script>
+  import Card from "../../lib/components/Card.svelte";
+  
+  import {cardInfo} from "$lib/data.js";
+</script>
+
+<section class="lg:grid grid-cols-2">
+  {#each cardInfo as card}
+    <Card {...card} />
+  {/each}
+</section>
+
+<!-- <section class="w-[50%] m-auto lg:grid grid-cols-2 p-4 pb-4 gap-4"></section>
  <div class="border-solid border-2 p-4 mb-4">  
     <h1>Blog Post 1</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
@@ -29,4 +41,4 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
   </div>
   
-</section>
+</section> -->
