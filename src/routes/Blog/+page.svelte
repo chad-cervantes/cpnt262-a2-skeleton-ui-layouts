742048e3218e8b5fd9edacb9a73dbea4c1569063
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-17 14:09:47 -0700

    used a ListBox called Native Alternatives as category tabs

--- src/routes/Blog/+page.svelte
+++ src/routes/Blog/+page.svelte
@@ -4,41 +4,19 @@
   import {cardInfo} from "$lib/data.js";
 </script>
 
+<h3 class="text-center text-2xl p-4">List of Blog Categories</h3>
+  <div class="w-[50%] m-auto p-4">
+  <select class="select" size="4" value="1">
+    <option value="1">Category 1</option>
+    <option value="2">Category 2</option>
+    <option value="3">Category 3</option>
+    <option value="4">Category 4</option>
+    <option value="5">Category 5</option>
+  </select>
+  </div>	
+
 <section class="lg:grid grid-cols-2">
   {#each cardInfo as card}
     <Card {...card} />
   {/each}
 </section>
-
-<!-- <section class="w-[50%] m-auto lg:grid grid-cols-2 p-4 pb-4 gap-4"></section>
- <div class="border-solid border-2 p-4 mb-4">  
-    <h1>Blog Post 1</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div> 
-
-  <div class="border-solid border-2 p-4 mb-4">
-    <h1>Blog Post 2</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div>
-  
-  <div class="border-solid border-2 p-4 mb-4">
-    <h1>Blog Post 3</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div>
-  
-  <div class="border-solid border-2 p-4 mb-4">
-  <h1>Blog Post 4</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div>
-
-  <div class="border-solid border-2 p-4 mb-4">
-    <h1>Blog Post 5</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div>
-  
-  <div class="border-solid border-2 p-4 mb-4">
-    <h1>Blog Post 6</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolorum temporibus ipsa maxime impedit est facilis recusandae error harum suscipit iure molestias quasi quaerat commodi, ducimus magni dolor consequuntur ullam.</p>
-  </div>
-  
-</section> -->
